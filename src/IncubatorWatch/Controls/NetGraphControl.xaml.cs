babdc85052ee0766d191445685ab48d0c8a4d060
eric.aubin <eric.aubin@olympusndt.com>
2013-01-23 17:35:12 -0500

    2 Graphs

--- src/IncubatorWatch/Controls/NetGraphControl.xaml.cs
+++ src/IncubatorWatch/Controls/NetGraphControl.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows;
 using System.Windows.Media;
 using System.Windows.Threading;
 using MahApps.Metro;
+using IncubatorWatch.Manager;
 using Microsoft.Research.DynamicDataDisplay;
 using Microsoft.Research.DynamicDataDisplay.DataSources;
 using Brushes = System.Windows.Media.Brushes;
